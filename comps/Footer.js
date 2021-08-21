56d78ebd9ec4e49b7879a1c70aa0858ab5889195
solan117 <karan.solanki69@gmail.com>
2021-08-21 11:48:20 -0400

    Created a template file for NextJS project

---
+++ comps/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return ( 
+        <div>
+            <h4>
+                CopyRight 2021 
+            </h4>
+        </div>
+     );
+}
+ 
+export default Footer;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+      <div>
+          <Navbar/>
+          <h1>HomePage</h1>
+          <Footer/>
+      </div>
+  )
+}
